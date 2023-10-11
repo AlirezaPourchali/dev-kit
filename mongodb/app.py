ffb3c55e41674ec915c5a3b720854884c69aa7a5
AlirezaPourchali <arp.joker82@gmail.com>
2023-10-11 15:26:28 +0330

    add: pymongo code for connecting to mongodb

---
+++ mongodb/app.py
@@ -0,0 +1,58 @@
+import pymongo
+
+CONNECTION = "mongodb://<user(root)>:<password>@<service_name>:27017"
+
+client = pymongo.MongoClient(CONNECTION)
+
+# print all databases
+
+print(client.list_database_names())
+
+
+# use a db
+
+mydb = client['<name_of_database>']
+
+# create document
+
+mydb.create_collection('hello')
+
+# list collection names
+
+mydb.list_collection_names()
+
+# use hello collection
+
+mycollection = mydb['hello']
+
+# insert one document
+
+item_1 = {
+  "_id" : "U1IT00001",
+  "item_name" : "Blender",
+  "max_discount" : "10%",
+  "batch_number" : "RR450020FRG",
+  "price" : 340,
+  "category" : "kitchen appliance"
+}
+
+mycollection.insert_one(item_1)
+
+# insert many documents
+
+item_2 = {
+  "_id" : "U1IT00002",
+  "item_name" : "Egg",
+  "category" : "food",
+  "quantity" : 12,
+  "price" : 36,
+  "item_description" : "brown country eggs"
+}
+
+item_3 = {
+  "item_name" : "Bread",
+  "quantity" : 2,
+  "ingredients" : "all-purpose flour" 
+}
+
+mycollection.insert_many([item_2,item_3])
